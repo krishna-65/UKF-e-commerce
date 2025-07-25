274ecd0fc816585a6726a2dbff21d6417d604d7b
krishna-65 <sharmakrishnakant721@gmail.com>
2025-07-25 10:58:37 +0530

    add profile

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuth, login, logout, register, updateProfile } from '../controllers/userController.js';
+import { getProfile, isAuth, login, logout, register, updateProfile } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 
 const userRouter = express.Router();
@@ -9,7 +9,6 @@ userRouter.post('/login', login)
 userRouter.put('/update/:id',updateProfile)
 userRouter.get('/is-auth', authUser, isAuth)
 userRouter.get('/logout', authUser, logout)
-
-
+userRouter.get('/profile/:id', getProfile)
 
 export default userRouter
