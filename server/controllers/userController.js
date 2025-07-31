b8d7ad3120c4c52129425c46391450b72edb442c
krishna-65 <sharmakrishnakant721@gmail.com>
2025-07-31 13:18:51 +0530

    increase limit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -340,7 +340,7 @@ export const getAdminDashboardStats = async (req, res) => {
 export const getUsers = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 25;
     const skip = (page - 1) * limit;
  
 
